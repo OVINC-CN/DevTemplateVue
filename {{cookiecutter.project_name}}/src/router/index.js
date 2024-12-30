b553db276abe6a5b3990a84f796471bc1410332d
orenzhang <orenzhang@ovinc.cn>
2024-12-30 14:03:52 +0800

    feat(account): always use union api token for user verify

--- {{cookiecutter.project_name}}/src/router/index.js
+++ {{cookiecutter.project_name}}/src/router/index.js
@@ -7,11 +7,6 @@ const routes = [
     component: () => import('../views/Home.vue'),
   },
   {
-    path: '/login/',
-    name: 'Login',
-    component: () => import('../views/Login.vue'),
-  },
-  {
 
     path: '/:pathMatch(.*)*',
     name: 'Notfound',
